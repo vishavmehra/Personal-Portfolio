d0c675f4d32caee6f80dff88b5e738f45eefd36a
vishavmehra <77605063+vishavmehra@users.noreply.github.com>
2024-03-06 18:36:31 +0530

    intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -95,7 +95,7 @@ export default function Intro() {
         <a
           className="group bg-white jelly px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="https://drive.google.com/file/d/1jpwZtUDsOiU25KpG_AhLp6eSla2ZAkaQ/view?usp=drive_link"
-          download
+          My resume
           target="_blank"  // Add this line
           rel="noopener noreferrer"
         >
